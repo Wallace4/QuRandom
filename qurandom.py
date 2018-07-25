02f1fbd32c00c9d9820e638915816b3a1ab5f925
Michele De Quattro <mikde4@gmail.com>
2018-07-25 19:55:26 +0200

    Update qurandom.py
    Indentation of comments

--- qurandom.py
+++ qurandom.py
@@ -34,23 +34,23 @@ n_digits = 3
 
 def qurandom (quantum_engine):
 	qubit = quantum_engine.allocate_qubit()		#allocate the qubit
-	H | qubit									#use the H port to give 1/2 of probability to be either 0 or 1
-	Measure | qubit								#mmeasure the qubit and let him collapse
+	H | qubit					#use the H port to give 1/2 of probability to be either 0 or 1
+	Measure | qubit					#mmeasure the qubit and let him collapse
 	random_number = int(qubit)
 	return random_number
 
-def get_random_number (n):						#n is the number of binary digits we want the number to be
+def get_random_number (n):					#n is the number of binary digits we want the number to be
 	random_number = 0
 	quantum_engine = MainEngine()				#creation of the Quantum Engine
 	for i in range(n):
 		random_number = random_number << 1		#move the last generate 1 or 0 into a more significant digit
 		quvalue = qurandom(quantum_engine)		#generation of 0 or 1
-		random_number += quvalue				#the last digit (the unit) is now the random generated numer
+		random_number += quvalue			#the last digit (the unit) is now the random generated numer
 	quantum_engine.flush()
 	return random_number
 
 def optimize_plot(iteration, lim, multiplier, global_lim):	#this is a function that help visualize the graph in a log scale
-															#taken from https://github.com/SqrtMinusOne/Euler-s-number/ and thanks to the author
+								#taken from https://github.com/SqrtMinusOne/Euler-s-number/ and thanks to the author
 	if iteration < lim and iteration % multiplier == 0:
 		return True
 	elif lim < global_lim:
@@ -58,8 +58,8 @@ def optimize_plot(iteration, lim, multiplier, global_lim):	#this is a function t
 	else:
 		return False
 
-def filewriter ():						#the function that calculate all the numbers and write them in the Histogram
-	count_list [] #list where i put all the random generated numbers
+def filewriter ():					#the function that calculate all the numbers and write them in the Histogram
+	count_list [] 					#list where i put all the random generated numbers
 	
 	fig = plt.figure()
 	
@@ -69,23 +69,23 @@ def filewriter ():						#the function that calculate all the numbers and write t
 	data = np.array(data)
 	
 	d = np.diff(np.unique(data)).min()
-	left_of_first_bin = data.min() - float(d)/2		#all this is black magic to align the hist better
+	left_of_first_bin = data.min() - float(d)/2	#all this is black magic to align the hist better
 	right_of_last_bin = data.max() + float(d)/2
 	
 	for i in range(N):
-		qrn = get_random_number(n_digits)			#generation of the number
+		qrn = get_random_number(n_digits)		#generation of the number
 		
-		count_list.append(qrn)						#append the number in the list
+		count_list.append(qrn)				#append the number in the list
 		
-		is_time_for_show = optimize_plot(i, 20, 1, N)	#se if the number is high enough to change the rateo of visualization
+		is_time_for_show = optimize_plot(i, 20, 1, N)	#check if the number is high enough to change the rateo of visualization
 		
-		if (is_time_for_show or i == N-1): #Histogram generation
+		if (is_time_for_show or i == N-1): 		#Histogram generation
 			total_axes.clear()
 			total_axes.hist(count_list, np.arange(left_of_first_bin, right_of_last_bin + d, d), color='purple')
 			total_axes.set_title("Number of generations = {0}".format(i))
 		
-			plt.savefig('figures/fig{0:5}'.format(i))	#save each frame into a different image to be then converted in a gif
+			plt.savefig('figures/fig{0:5}'.format(i))#save each frame into a different image to be then converted in a gif
 		
 	plt.show()
 
-filewriter ()			#call of the main function
+filewriter ()	#call of the main function
